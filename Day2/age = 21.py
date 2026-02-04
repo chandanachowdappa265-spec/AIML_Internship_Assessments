dc3566c16ffa347208b83ed5da84aba851c800c6
chandana <chandanachowdappa265@gmail.com>
2026-02-04 22:50:10 +0530

    Add Day2 as normal folder

---
+++ Day2/age = 21.py
@@ -0,0 +1,9 @@
+age = 20
+height = 4.5
+name = "chandana"
+is_student = True
+
+print(type (age))
+print(type (height))
+print(type (name))
+print(type (is_student))
---
+++ Day2/python_fundamental.py
@@ -0,0 +1,10 @@
+name = "chandana"   
+age = 21
+
+price = 5.5
+is_student = True
+
+print(name, age, price, is_student)
+
+print(type(name))
+print(type(age))
