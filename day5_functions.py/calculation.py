76fc0a590bfeb357c7234d59d24a4d72551cc291
chandana <chandanachowdappa265@gmail.com>
2026-02-06 10:44:26 +0530

    Added day4_dict_sets.py folder

---
+++ day5_functions.py/calculation.py
@@ -0,0 +1,10 @@
+
+class calculation:
+    def add(self,a,b):
+        return a +b
+    def product(self,a,b):
+        return a*b
+    def subtract(self,a,b):
+        return a-b
+    def power(self,a,b):
+        return a**b
